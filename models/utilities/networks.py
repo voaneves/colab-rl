1d13cce075b27d49aa0182758c9547a2166aeb1f
Neves4 <victorneves478@gmail.com>
2018-11-04 15:14:11 -0200

    Make the networks modular with Keras Functional module

--- models/utilities/networks.py
+++ models/utilities/networks.py
@@ -13,20 +13,7 @@ __license__ = "MIT"
 __maintainer__ = "Victor Neves"
 __email__ = "victorneves478@gmail.com"
 
-def CNN1(optimizer, loss, stack, input_size, output_size):
-    model = Sequential()
-    model.add(Conv2D(16, (3, 3), activation = 'relu', input_shape = (stack,
-                                                                     input_size,
-                                                                     input_size)))
-    model.add(Conv2D(32, (3, 3), activation = 'relu'))
-    model.add(Flatten())
-    model.add(Dense(256, activation = 'relu'))
-    model.add(Dense(output_size))
-    model.compile(optimizer = optimizer, loss = loss)
-
-    return model
-
-def CNN2(optimizer, loss, stack, input_size, output_size, min_neurons = 16,
+def weird_CNN(optimizer, loss, stack, input_size, output_size, min_neurons = 16,
          max_neurons = 128, kernel_size = (3,3), layers = 4):
     # INPUTS
     # size     - size of the input images
@@ -74,72 +61,61 @@ def CNN2(optimizer, loss, stack, input_size, output_size, min_neurons = 16,
 
     return model
 
-def CNN3(optimizer, loss, stack, input_size, output_size, drop_prob = 0.1):
-    model = Sequential()
-    model.add(Conv2D(16, (3, 3), activation = 'relu',input_shape = (stack,
-                                                                    input_size,
-                                                                    input_size)))
-    model.add(Conv2D(32, (3, 3), activation = 'relu'))
-    model.add(Conv2D(32, (3, 3), activation = 'relu'))
-    model.add(MaxPooling2D(pool_size = (2, 2)))
-    model.add(Flatten())
-    model.add(Dense(256, activation = 'relu'))
-    model.add(Dropout(drop_prob))
-    model.add(Dense(output_size))
-    model.compile(optimizer = optimizer, loss = loss)
-
-    return model
-
-def CNN4(optimizer, loss, stack, input_size, output_size):
-    """From @Kaixhin implementation's of the Rainbow paper."""
-    model = Sequential()
-    model.add(Conv2D(32, 8, (4, 4), activation = 'relu',input_shape = (stack,
-                                                                    input_size,
-                                                                    input_size)))
-    model.add(Conv2D(64, 4, (2, 2), activation = 'relu'))
-    model.add(Conv2D(64, 3, (2, 2), activation = 'relu'))
-    model.add(Flatten())
-    model.add(Dense(3136, activation = 'relu'))
-    model.add(Dense(output_size))
-    model.compile(optimizer = optimizer, loss = loss)
-
-    return model
-
-def CNN_DUELING(optimizer, loss, stack, input_size, output_size):
-    inputs = Input(shape = (stack, input_size, input_size))
+def CNN1(inputs, stack, input_size):
     net = Conv2D(16, (3, 3), activation = 'relu')(inputs)
     net = Conv2D(32, (3, 3), activation = 'relu')(net)
-    net = Flatten()(net)
-    advt = Dense(256, activation = 'relu')(net)
-    advt = Dense(output_size)(advt)
-    value = Dense(256, activation = 'relu')(net)
-    value = Dense(1)(value)
-    # now to combine the two streams
-    advt = Lambda(lambda advt: advt - tf.reduce_mean(advt, axis = -1,
-                                                     keepdims = True))(advt)
-    value = Lambda(lambda value: tf.tile(value, [1, output_size]))(value)
-    final = Add()([value, advt])
-    model = Model(inputs = inputs, outputs = final)
-    model.compile(optimizer = optimizer, loss = loss)
 
     return model
 
-def CNN_TEST_LOCAL(optimizer, global_state_shape, local_state_shape, loss, output_size):
-    global_state = Sequential()
-    global_state.add(Conv2D(16, activation = 'relu', input_shape = frames_shape))
+def CNN2(inputs, stack, input_size):
+    net = Conv2D(16, (3, 3), activation = 'relu')(inputs)
+    net = Conv2D(32, (3, 3), activation = 'relu')(net)
+    net = Conv2D(32, (3, 3), activation = 'relu')(net)
+    net = MaxPooling2D(pool_size = (2, 2))(net)
+    net = Flatten()(net)
 
-    local_state = Sequential()
-    local_state.add(Conv2D(16, activation = 'relu', input_shape = local_state_shape))
+    return model
 
-    merge_inputs = Merge([global_state, local_state], mode = 'concat')
+def CNN3(inputs, stack, input_size):
+    """From @Kaixhin implementation's of the Rainbow paper."""
+    net = Conv2D(32, (4, 4), activation = 'relu')(inputs)
+    net = Conv2D(64, (2, 2), activation = 'relu')(net)
+    net = Conv2D(64, (2, 2), activation = 'relu')(net)
+    net = Flatten()(net)
 
-    model = Sequential()
-    model.add(merge_inputs)
-    model.add(Conv2D(16, (3, 3), activation = 'relu'))
-    model.add(Conv2D(32, (3, 3), activation = 'relu'))
-    model.add(Flatten())
-    model.add(Dense(256, activation = 'relu'))
-    model.add(Dense(output_size))
+    return net
+
+def create_cnn(cnn, inputs, stack, input_size):
+    if cnn == "CNN1":
+        net = CNN1(inputs, stack, input_size)
+    elif cnn == "CNN2":
+        net = CNN2(inputs, stack, input_size)
+    else:
+        net = CNN3(inputs, stack, input_size)
+
+    return net
+
+def create_model(optimizer, loss, stack, input_size, output_size,
+                  dueling = False, cnn = "CNN3"):
+    inputs = Input(shape = (stack, input_size, input_size))
+    net = create_cnn(cnn, inputs, stack, input_size)
+
+    if dueling:
+        advt = Dense(3136, activation = 'relu')(net)
+        advt = Dense(output_size)(advt)
+        value = Dense(3136, activation = 'relu')(net)
+        value = Dense(1)(value)
+
+        # now to combine the two streams
+        advt = Lambda(lambda advt: advt - tf.reduce_mean(advt, axis = -1,
+                                                         keepdims = True))(advt)
+        value = Lambda(lambda value: tf.tile(value, [1, output_size]))(value)
+        final = Add()([value, advt])
+    else:
+        final = Dense(3136, activation = 'relu')(net)
+        final = Dense(output_size)(final)
+
+    model = Model(inputs = inputs, outputs = final)
     model.compile(optimizer = optimizer, loss = loss)
 
     return model
