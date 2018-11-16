0b792f4fe8354911a3a8ec5244eabea2a74b5209
Neves4 <victorneves478@gmail.com>
2018-11-15 23:44:31 -0200

    Moved LinearSchedule to misc.py

--- models/utilities/misc.py
+++ models/utilities/misc.py
@@ -1,15 +1,41 @@
 #!/usr/bin/env python
+
 import numpy as np
 from argparse import ArgumentParser
 
 from keras import backend as K
-import keras.optimizers as optimizers
 import tensorflow as tf
 
 __author__ = "Victor Neves"
 __license__ = "MIT"
 __maintainer__ = "Victor Neves"
 __email__ = "victorneves478@gmail.com"
+
+
+class LinearSchedule(object):
+    def __init__(self, schedule_timesteps, final_p, initial_p):
+        """Linear interpolation between initial_p and final_p over
+        schedule_timesteps. After this many timesteps pass final_p is
+        returned.
+        Parameters
+        ----------
+        schedule_timesteps: int
+            Number of timesteps for which to linearly anneal initial_p
+            to final_p
+        initial_p: float
+            initial output value
+        final_p: float
+            final output value
+        """
+        self.schedule_timesteps = schedule_timesteps
+        self.final_p = final_p
+        self.initial_p = initial_p
+
+    def value(self, t):
+        """See Schedule.value"""
+        fraction = min(float(t) / self.schedule_timesteps, 1.0)
+        return self.initial_p + fraction * (self.final_p - self.initial_p)
+
 
 class HandleArguments:
         """Handle arguments provided in the command line when executing the model.
@@ -22,47 +48,25 @@ class HandleArguments:
             NEED UPDATE!
         """
         def __init__(self):
-            self.parser = ArgumentParser() # Receive arguments
-            self.parser.add_argument("-l", "--load", help = "load a previously trained model. the argument is the filename", required = False, default = "")
-            self.parser.add_argument("-v", "--visual", help = "define board size", required = False, action = 'store_true')
-            self.parser.add_argument("-du", "--dueling", help = "use dueling DQN", required = False, action = 'store_true')
-            self.parser.add_argument("-do", "--double", help = "use double DQN", required = False, action = 'store_true')
-            self.parser.add_argument("-p", "--per", help = "use Prioritized Experience Replay", required = False, action = 'store_true')
-            self.parser.add_argument("-ls", "--local_state", help = "define board size", required = False, action = 'store_true')
-            self.parser.add_argument("-g", "--board_size", help = "define board size", required = False, default = 10, type = int)
-            self.parser.add_argument("-nf", "--nb_frames", help = "define board size", required = False, default = 4, type = int)
-            self.parser.add_argument("-na", "--nb_actions", help = "define board size", required = False, default = 5, type = int)
-            self.parser.add_argument("-uf", "--update_freq", help = "frequency to update target", required = False, default = 500, type = int)
+            self.parser = ArgumentParser()
+            self.parser.add_argument("-load", help = "load a previously trained model. the argument is the filename", required = False, default = "", type = str)
+            self.parser.add_argument("-cnn_model", help = "choose the cnn model used", required = False, default = "CNN3", type = str)
+            self.parser.add_argument("-optimizer", help = "choose the optimizer for the cnn model", required = False, default = "RMSprop", type = str)
+            self.parser.add_argument("-error", help = "choose the loss function for the cnn model", required = False, default = "clipped_error", type = str)
+            self.parser.add_argument("--visual", help = "define board size", required = False, action = 'store_true')
+            self.parser.add_argument("--dueling", help = "use dueling DQN", required = False, action = 'store_true')
+            self.parser.add_argument("--double", help = "use double DQN", required = False, action = 'store_true')
+            self.parser.add_argument("--per", help = "use Prioritized Experience Replay", required = False, action = 'store_true')
+            self.parser.add_argument("--noisy_net", help = "use Prioritized Experience Replay", required = False, action = 'store_true')
+            self.parser.add_argument("--local_state", help = "define board size", required = False, action = 'store_true')
+            self.parser.add_argument("--benchmark", help = "test the trained model", required = False, action = 'store_true')
+            self.parser.add_argument("-n_steps", help = "choose the ammount of steps in Multi-step returns", required = False, default = 1, type = int)
+            self.parser.add_argument("-board_size", help = "define board size", required = False, default = 10, type = int)
+            self.parser.add_argument("-memory_size", help = "define the ammount of episodes to remember", required = False, default = -1, type = int)
+            self.parser.add_argument("-nb_frames", help = "define board size", required = False, default = 4, type = int)
+            self.parser.add_argument("-update_freq", help = "frequency to update target", required = False, default = 500, type = int)
 
-            self.args = self.parser.parse_args()
-            self.status_load = False
-            self.status_visual = False
-            self.local_state = False
-            self.dueling = False
-            self.double = False
-            self.per = False
-
-            if self.args.load:
-                script_dir = path.dirname(__file__) # Absolute dir the script is in
-                abs_file_path = path.join(script_dir, self.args.load)
-                model = load_model(abs_file_path)
-
-                self.status_load = True
-
-            if self.args.visual:
-                self.status_visual = True
-
-            if self.args.local_state:
-                self.local_state = True
-
-            if self.args.dueling:
-                self.dueling = True
-
-            if self.args.double:
-                self.double = True
-
-            if self.args.per:
-                self.per = True
+            self.args = self.parser.parse_args() # Receive arguments
 
 def huber_loss(y_true, y_pred, clip_value):
 	# Huber loss, see https://en.wikipedia.org/wiki/Huber_loss and
--- models/utilities/policy.py
+++ models/utilities/policy.py
@@ -1,30 +1,7 @@
 import random
 import numpy as np
 
-class LinearSchedule(object):
-    def __init__(self, schedule_timesteps, final_p, initial_p):
-        """Linear interpolation between initial_p and final_p over
-        schedule_timesteps. After this many timesteps pass final_p is
-        returned.
-        Parameters
-        ----------
-        schedule_timesteps: int
-            Number of timesteps for which to linearly anneal initial_p
-            to final_p
-        initial_p: float
-            initial output value
-        final_p: float
-            final output value
-        """
-        self.schedule_timesteps = schedule_timesteps
-        self.final_p = final_p
-        self.initial_p = initial_p
-
-    def value(self, t):
-        """See Schedule.value"""
-        fraction = min(float(t) / self.schedule_timesteps, 1.0)
-        return self.initial_p + fraction * (self.final_p - self.initial_p)
-
+from .misc import LinearSchedule
 
 class GreedyQPolicy:
     """Implement the greedy policy
