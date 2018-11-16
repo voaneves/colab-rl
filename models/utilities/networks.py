2bdaa0a64395aa9389629af8cf71a3313bc04a7f
Neves4 <victorneves478@gmail.com>
2018-11-15 23:50:58 -0200

    Adding noisy models as a network option. NoisyDense implementation is from @spring01

--- models/utilities/networks.py
+++ models/utilities/networks.py
@@ -4,101 +4,85 @@
 """
 
 import numpy as np
-from keras.models import Sequential, load_model, Model
-from keras.layers import *
 import tensorflow as tf
+
+from keras import backend as K
+from keras.optimizers import RMSprop, Nadam
+from keras.models import Sequential, load_model, Model
+from keras.layers import Conv2D, Dense, Flatten, MaxPooling2D, Flatten, Input,\
+                         Lambda, Add
+from .noisy_dense import NoisyDenseFG
+from .misc import clipped_error
 
 __author__ = "Victor Neves"
 __license__ = "MIT"
 __maintainer__ = "Victor Neves"
 __email__ = "victorneves478@gmail.com"
 
-def weird_CNN(optimizer, loss, stack, input_size, output_size, min_neurons = 16,
-         max_neurons = 128, kernel_size = (3,3), layers = 4):
-    # INPUTS
-    # size     - size of the input images
-    # n_layers - number of layers
-    # OUTPUTS
-    # model    - compiled CNN
+K.set_image_dim_ordering('th')  # Setting keras ordering
 
-    # Define hyperparamters
-    MIN_NEURONS = min_neurons
-    MAX_NEURONS = max_neurons
-    KERNEL = kernel_size
-    n_layers = layers
+def select_optimizer(optimizer):
+    assert optimizer in {'Nadam', 'RMSprop'}, "Optimizer should be RMSprop or Nadam."
 
-    # Determine the # of neurons in each convolutional layer
-    steps = np.floor(MAX_NEURONS / (n_layers + 1))
-    neurons = np.arange(MIN_NEURONS, MAX_NEURONS, steps)
-    neurons = neurons.astype(np.int32)
+    if optimizer == 'Nadam':
+        optimizer = Nadam()
+    else:
+        optimizer = RMSprop()
 
-    # Define a model
-    model = Sequential()
+    return optimizer
 
-    # Add convolutional layers
-    for i in range(0, n_layers):
-        if i == 0:
-            model.add(Conv2D(neurons[i], KERNEL, input_shape = (stack,
-                                                                input_size,
-                                                                input_size)))
-        else:
-            model.add(Conv2D(neurons[i], KERNEL))
+def select_error(error):
+    assert type(error) is str, "Should use string to select error."
 
-        model.add(Activation('relu'))
+    if error == 'clipped_error':
+        error = clipped_error
 
-    # Add max pooling layer
-    model.add(MaxPooling2D(pool_size = (2, 2)))
-    model.add(Flatten())
-    model.add(Dense(MAX_NEURONS * 4))
-    model.add(Activation('relu'))
+    return error
 
-    # Add output layer
-    model.add(Dense(output_size))
-    model.add(Activation('sigmoid'))
-
-    # Compile the model
-    model.compile(loss = loss, optimizer = optimizer)
-
-    return model
-
-def CNN1(inputs, stack, input_size):
+def CNN1(inputs):
     net = Conv2D(16, (3, 3), activation = 'relu')(inputs)
     net = Conv2D(32, (3, 3), activation = 'relu')(net)
 
     return model
 
-def CNN2(inputs, stack, input_size):
+def CNN2(inputs):
     net = Conv2D(16, (3, 3), activation = 'relu')(inputs)
     net = Conv2D(32, (3, 3), activation = 'relu')(net)
     net = Conv2D(32, (3, 3), activation = 'relu')(net)
-    net = MaxPooling2D(pool_size = (2, 2))(net)
     net = Flatten()(net)
 
     return model
 
-def CNN3(inputs, stack, input_size):
+def CNN3(inputs):
     """From @Kaixhin implementation's of the Rainbow paper."""
-    net = Conv2D(32, (4, 4), activation = 'relu')(inputs)
+    net = Conv2D(32, (3, 3), activation = 'relu')(inputs)
     net = Conv2D(64, (2, 2), activation = 'relu')(net)
-    net = Conv2D(64, (2, 2), activation = 'relu')(net)
+    net = Conv2D(64, (1, 1), activation = 'relu')(net)
     net = Flatten()(net)
 
     return net
 
-def create_cnn(cnn, inputs, stack, input_size):
+def create_cnn(cnn, inputs):
     if cnn == "CNN1":
-        net = CNN1(inputs, stack, input_size)
+        net = CNN1(inputs)
     elif cnn == "CNN2":
-        net = CNN2(inputs, stack, input_size)
+        net = CNN2(inputs)
     else:
-        net = CNN3(inputs, stack, input_size)
+        net = CNN3(inputs)
 
     return net
 
 def create_model(optimizer, loss, stack, input_size, output_size,
-                  dueling = False, cnn = "CNN3"):
+                  dueling = False, cnn = "CNN3", noisy = False):
+    if noisy:
+        return create_noisy_model(optimizer, loss, input_size, output_size,
+                                  dueling, cnn, noisy)
+
+    optimizer = select_optimizer(optimizer)
+    loss = select_error(loss)
+
     inputs = Input(shape = (stack, input_size, input_size))
-    net = create_cnn(cnn, inputs, stack, input_size)
+    net = create_cnn(cnn, inputs)
 
     if dueling:
         advt = Dense(3136, activation = 'relu')(net)
@@ -117,5 +101,35 @@ def create_model(optimizer, loss, stack, input_size, output_size,
 
     model = Model(inputs = inputs, outputs = final)
     model.compile(optimizer = optimizer, loss = loss)
+
+    return model
+
+def create_noisy_model(optimizer, loss, stack, input_size, output_size,
+                  dueling = False, cnn = "CNN3"):
+    loss = select_error(loss)
+
+    inputs = tf.keras.layers.Input(shape = (stack, input_size, input_size))
+    net = tf.keras.layers.Conv2D(32, (3, 3), activation = 'relu')(inputs)
+    net = tf.keras.layers.Conv2D(64, (2, 2), activation = 'relu')(net)
+    net = tf.keras.layers.Conv2D(64, (1, 1), activation = 'relu')(net)
+    net = tf.keras.layers.Flatten()(net)
+
+    if dueling:
+        advt = NoisyDenseFG(3136, activation = 'relu')(net)
+        advt = NoisyDenseFG(output_size)(advt)
+        value = NoisyDenseFG(3136, activation = 'relu')(net)
+        value = NoisyDenseFG(1)(value)
+
+        # now to combine the two streams
+        advt = Lambda(lambda advt: advt - tf.reduce_mean(advt, axis = -1,
+                                                         keepdims = True))(advt)
+        value = Lambda(lambda value: tf.tile(value, [1, output_size]))(value)
+        final = Add()([value, advt])
+    else:
+        final = NoisyDenseFG(3136, activation = 'relu')(net)
+        final = NoisyDenseFG(output_size)(final)
+
+    model = tf.keras.models.Model(inputs = inputs, outputs = final)
+    model.compile(optimizer = tf.keras.optimizers.RMSprop(), loss = loss)
 
     return model
---
+++ models/utilities/noisy_dense.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.framework import tensor_shape
+from tensorflow.python.layers import base
+from tensorflow.python.ops.init_ops import Constant
+
+class NoisyDense(tf.keras.layers.Dense):
+
+    def build(self, input_shape):
+        input_shape = tensor_shape.TensorShape(input_shape)
+        if input_shape[-1].value is None:
+            raise ValueError('The last dimension of the inputs to `Dense` '
+                             'should be defined. Found `None`.')
+        self.input_spec = base.InputSpec(min_ndim=2,
+                                         axes={-1: input_shape[-1].value})
+        kernel_shape = [input_shape[-1].value, self.units]
+        kernel_quiet = self.add_variable('kernel_quiet',
+                                         shape=kernel_shape,
+                                         initializer=self.kernel_initializer,
+                                         regularizer=self.kernel_regularizer,
+                                         constraint=self.kernel_constraint,
+                                         dtype=self.dtype,
+                                         trainable=True)
+        scale_init = Constant(value=(0.5 / np.sqrt(kernel_shape[0])))
+        kernel_noise_scale = self.add_variable('kernel_noise_scale',
+                                               shape=kernel_shape,
+                                               initializer=scale_init,
+                                               dtype=self.dtype,
+                                               trainable=True)
+        kernel_noise = self.make_kernel_noise(shape=kernel_shape)
+        self.kernel = kernel_quiet + kernel_noise_scale * kernel_noise
+        if self.use_bias:
+            bias_shape = [self.units,]
+            bias_quiet = self.add_variable('bias_quiet',
+                                           shape=bias_shape,
+                                           initializer=self.bias_initializer,
+                                           regularizer=self.bias_regularizer,
+                                           constraint=self.bias_constraint,
+                                           dtype=self.dtype,
+                                           trainable=True)
+            bias_noise_scale = self.add_variable(name='bias_noise_scale',
+                                                 shape=bias_shape,
+                                                 initializer=scale_init,
+                                                 dtype=self.dtype,
+                                                 trainable=True)
+            bias_noise = self.make_bias_noise(shape=bias_shape)
+            self.bias = bias_quiet + bias_noise_scale * bias_noise
+        else:
+            self.bias = None
+        self.built = True
+
+    def make_kernel_noise(self, shape):
+        raise NotImplementedError
+
+    def make_bias_noise(self, shape):
+        raise NotImplementedError
+
+
+class NoisyDenseIG(NoisyDense):
+    '''
+    Noisy dense layer with independent Gaussian noise
+    '''
+    def make_kernel_noise(self, shape):
+        noise = tf.random_normal(shape, dtype=self.dtype)
+        kernel_noise = tf.Variable(noise, trainable=False, dtype=self.dtype)
+        self.noise_list = [kernel_noise]
+        return kernel_noise
+
+    def make_bias_noise(self, shape):
+        noise = tf.random_normal(shape, dtype=self.dtype)
+        bias_noise = tf.Variable(noise, trainable=False, dtype=self.dtype)
+        self.noise_list.append(bias_noise)
+        return bias_noise
+
+
+class NoisyDenseFG(NoisyDense):
+    '''
+    Noisy dense layer with factorized Gaussian noise
+    '''
+    def make_kernel_noise(self, shape):
+        kernel_noise_input = self.make_fg_noise(shape=[shape[0]])
+        kernel_noise_output = self.make_fg_noise(shape=[shape[1]])
+        self.noise_list = [kernel_noise_input, kernel_noise_output]
+        kernel_noise = kernel_noise_input[:, tf.newaxis] * kernel_noise_output
+        return kernel_noise
+
+    def make_bias_noise(self, shape):
+        return self.noise_list[1] # kernel_noise_output
+
+    def make_fg_noise(self, shape):
+        noise = tf.random_normal(shape, dtype=self.dtype)
+        trans_noise = tf.sign(noise) * tf.sqrt(tf.abs(noise))
+        return tf.Variable(trans_noise, trainable=False, dtype=self.dtype)
